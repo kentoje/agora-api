e999e5ca97a3ed2d64873918702244ffd58be998
kentoje <k.monthubert@gmail.com>
2020-06-25 15:39:24 +0200

    feat(cron): Add cron reset level

--- src/Command/ResetAllUserLevelCommand.php
+++ src/Command/ResetAllUserLevelCommand.php
@@ -34,7 +34,8 @@ class ResetAllUserLevelCommand extends Command
         $firstDayOfYear = new DateTime('first day of january');
         $currentDay = new DateTime();
 
-        if ($firstDayOfYear->format('Y-m-d') === $currentDay->format('Y-m-d')) {
+//        if ($firstDayOfYear->format('Y-m-d') === $currentDay->format('Y-m-d')) {
+        if (true) {
             $levelRepo = $this->em->getRepository(Level::class);
             $userRepo = $this->em->getRepository(User::class);
 
